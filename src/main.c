3ae74a834ce4acf6dc22998a2cb0f3324759e95a
P-B3117 <zacse38@gmail.com>
2026-07-06 13:10:35 -0400

    insane juju gere

--- src/main.c
+++ src/main.c
@@ -12,8 +12,7 @@
 
 #include "crc.h"
 #include "rmt.h"
-typedef struct
-{
+typedef struct {
   char *configString;
   int configInt;
 } taskConfig;
@@ -24,16 +23,14 @@ const uint8_t end = 0x7E;
 const uint8_t enteteLength = 4;
 const uint8_t crc = 2;
 
-typedef enum
-{
+typedef enum {
   debut = 0x01,
   data = 0x02,
   fin = 0x03,
   nack = 0x04,
 } com_typ;
 
-typedef enum
-{
+typedef enum {
   preambule_pos = 0,
   start_pos = 1,
   entete_pos = 2,
@@ -41,12 +38,9 @@ typedef enum
 } trame_pos; // crc and end pos depends on charge longueur
 
 #pragma pack(push, 1)
-typedef struct
-{
-  union
-  {
-    struct
-    {
+typedef struct {
+  union {
+    struct {
       uint8_t entete[4]; // type de comm, num sequence, longueur charge, volume
                          // dynamique
       uint8_t chargeUtile[80];
@@ -59,10 +53,8 @@ typedef struct
 } trame;
 #pragma pack(pop)
 
-void init_trame(trame *t)
-{
-  if (t != NULL)
-  {
+void init_trame(trame *t) {
+  if (t != NULL) {
     memset(t, 0, sizeof(trame));
   }
 }
@@ -71,8 +63,7 @@ uint8_t crc_pos(trame *t) { return (charge_pos + t->chargeLength); }
 
 uint8_t end_pos(trame *t) { return (charge_pos + t->chargeLength + 2); }
 
-typedef struct
-{
+typedef struct {
   uint8_t bitIndex;
   uint8_t messageIndex;
   uint8_t message[89];
@@ -81,58 +72,57 @@ typedef struct
   bool finished;
 } man_message;
 
-uint8_t print_man_message(man_message *mm)
-{
-  printf("MM:\n  bitIndex: %hhu\n  messageIndex: %hhu\n ", mm->bitIndex,
+uint8_t print_man_message(man_message *mm) {
+  printf("MM:\n  bitIndex: 0x%hhu\n  messageIndex: 0x%hhu\n ", mm->bitIndex,
          mm->messageIndex);
   printf("  Message[]:");
-  for (uint8_t i = 0; i < mm->messageIndex; i++)
-  {
-    if (i % 8 == 0)
-    {
+  for (uint8_t i = 0; i < mm->messageIndex; i++) {
+    if (i % 8 == 0) {
       printf("\n    ");
     }
-    printf(" %hhu,", mm->message[i]);
+    printf(" %02X,", mm->message[i]);
   }
   printf("\n  currentIndex: %hhu", mm->currentIndex);
-  printf("\n  current[]: %hhu, %hhu", mm->current[0], mm->current[1]);
+  printf("\n  current[]: 0x%02X, 0x%02X", mm->current[0], mm->current[1]);
   printf("\n  finished: %s\n", mm->finished ? "true" : "false");
 
   return 0;
 }
 
-uint8_t init_man_message(man_message *mm)
-{
-  if (mm != NULL)
-  {
+uint8_t init_man_message(man_message *mm) {
+  if (mm != NULL) {
     memset(mm, 0, sizeof(man_message));
   }
 
   return 0;
 }
 
-void follow(man_message *mm, uint8_t duration, bool level)
-{
+void follow(man_message *mm, uint8_t duration, bool level) {
 
   if (mm->finished)
     return;
 
   // if long pulse, treated as 2 short pulses
-  if (duration > RMT_FULL_BIT_THRESHOLD)
+  if (duration == 2) // TODO define long and short durations
   {
-    follow(mm, duration / 2, level);
-    follow(mm, duration / 2, level);
+    follow(mm, 1, level);
+    follow(mm, 1, level);
     return;
+  } else if (duration == 0) {
+    return;
+  } else if (duration > 2) {
+      return;
   }
+  // if very long pulse, its the first value of the next (edge case?)
+
+  // printf("\nFollowing %hhu, %d\n", duration, level);
 
   // function logic
   mm->current[mm->currentIndex] = level;
 
-  if (mm->currentIndex == 1)
-  {
+  if (mm->currentIndex == 1) {
     // logic
-    if (mm->current[1])
-    {
+    if (mm->current[1]) {
       mm->message[mm->messageIndex] |= (1 << (7 - mm->bitIndex)); // MSB
     }
 
@@ -141,16 +131,12 @@ void follow(man_message *mm, uint8_t duration, bool level)
     mm->currentIndex = 0;
     mm->current[0] = 0;
     mm->current[1] = 0;
-  }
-  else
-  {
+  } else {
     mm->currentIndex++;
   }
 
-  if (mm->bitIndex >= 8)
-  {
-    if (mm->message[mm->messageIndex] == end && mm->messageIndex >= 8)
-    {
+  if (mm->bitIndex >= 8) {
+    if (mm->message[mm->messageIndex] == end && mm->messageIndex >= 8) {
       mm->finished = true;
     }
 
@@ -161,40 +147,34 @@ void follow(man_message *mm, uint8_t duration, bool level)
 }
 
 // TODO currently copies the mm to the trame, could be improved.
-void man_to_trame(man_message *mm, trame *out)
-{
+void man_to_trame(man_message *mm, trame *out) {
   out->chargeLength = mm->messageIndex - 7;
   memcpy(out->flat_buffer, &mm->message[2], mm->messageIndex - 4);
   memcpy(out->crc, &mm->message[mm->messageIndex - 3], 2);
 }
 
+// currently assumes it receives a byte.
 static void decode_manchester_charles(rmt_symbol_word_t *sym, trame *out,
-                                      man_message *mm)
-{
-  for (uint8_t i = 0; i < 8; i++)
-  {
+                                      man_message *mm, uint8_t n_sym) {
+  for (uint8_t i = 0; i < n_sym; i++) {
     follow(mm, sym[i].duration0, sym[i].level0);
     follow(mm, sym[i].duration1, sym[i].level1);
   }
 }
 
-static uint8_t decode_manchester(rmt_symbol_word_t *sym)
-{
+static uint8_t decode_manchester(rmt_symbol_word_t *sym) {
   uint8_t out = 0;
 
-  for (int i = 0; i < 8; i++)
-  {
+  for (int i = 0; i < 8; i++) {
     rmt_symbol_word_t s = sym[i];
     bool bit;
 
     // LOW→HIGH = 1
-    if (s.level0 == 0 && s.level1 == 1)
-    {
+    if (s.level0 == 0 && s.level1 == 1) {
       bit = 1;
     }
     // HIGH→LOW = 0
-    else
-    {
+    else {
       bit = 0;
     }
 
@@ -204,38 +184,32 @@ static uint8_t decode_manchester(rmt_symbol_word_t *sym)
   return out;
 }
 
-void initTaskConfig(taskConfig *conf, char *str, int num)
-{
+void initTaskConfig(taskConfig *conf, char *str, int num) {
   conf->configString = str;
   conf->configInt = num;
 }
 
-void task(void *pvParameters)
-{
+void task(void *pvParameters) {
   taskConfig *conf = (taskConfig *)pvParameters;
 
-  while (1)
-  {
+  while (1) {
     printf("tasking");
     vTaskDelay(pdMS_TO_TICKS(500));
   }
 }
 
 // loop send toutes les trames dans une queue.
-void task_sender(void *pvParameters)
-{
+void task_sender(void *pvParameters) {
   ////RMT
 }
 
 // loop receive toutes les trames dans une queue
-void task_receiver(void *pvParameters)
-{
+void task_receiver(void *pvParameters) {
   ////RMT
 }
 
 // crc is 2 byte array (to ensure easy concatenation)
-void calc_crc(trame *input)
-{
+void calc_crc(trame *input) {
   uint16_t crc = crc16(input->flat_buffer, input->chargeLength + 4);
   input->crc[0] = crc & 0xFF;
   input->crc[1] = (crc >> 8) & 0xFF;
@@ -248,15 +222,18 @@ void send(trame *input) {}
 // decodes the buffer from manchester to standard bytes
 void receive(uint8_t *buffer, uint8_t start, uint8_t end) {}
 
-void rx_task(void *arg)
-{
+void rx_task(void *arg) {
   SemaphoreHandle_t sem = get_rx_semaphore();
   rmt_symbol_word_t local_symbols[BUFFER_SIZE];
+  trame tr;
+  man_message mm;
+  init_trame(&tr);
+  init_man_message(&mm);
 
-  for (;;)
-  {
-    if (xSemaphoreTake(sem, portMAX_DELAY) == pdTRUE)
-    {
+  printf("RX: now looping\n");
+
+  for (;;) {
+    if (xSemaphoreTake(sem, portMAX_DELAY) == pdTRUE) {
       get_rx_symbols(local_symbols, BUFFER_SIZE);
 
       for (int i = 0; i < BUFFER_SIZE; i++)
@@ -266,18 +243,20 @@ void rx_task(void *arg)
                local_symbols[i].level1, local_symbols[i].duration1);
       }
 
+      decode_manchester_charles(local_symbols, &tr,
+                                &mm, BUFFER_SIZE);
+      print_man_message(&mm);
+
       start_rx(); // re-arm for next reception
     }
     vTaskDelay(0);
   }
 }
 
-void tx_task(void *arg)
-{
+void tx_task(void *arg) {
   uint8_t data = 0;
 
-  for (;;)
-  {
+  for (;;) {
     send_byte(0x55);
     send_byte(data);
     data++;
@@ -285,8 +264,7 @@ void tx_task(void *arg)
   }
 }
 
-void app_main()
-{
+void app_main() {
   setup_transmission();
 
   printf("Starting tasks rx...\n");
